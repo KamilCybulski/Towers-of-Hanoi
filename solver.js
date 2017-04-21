a4e527e105ff397976b5d1fde593d71bb75b338b
Kamyl <kamillcybulski@gmail.com>
2017-04-21 23:48:04 +0200

    Upgraded data structures.

--- solver.js
+++ solver.js
@@ -1,63 +1,63 @@
 class Disc {
-    constructor(){
-        this.size = (++new.target.count || (new.target.count = 1));
+    constructor(size){
+        this.size = size
         this.color = this.size % 2 == 0 ? "red" : "blue";
     }
 }
 
-class Stack {
-    constructor(){
-        this.arr = [];
-    }
-    push(val){
-        this.arr.push(val)
-        return val;
-    }
-    pop(){
-        return this.arr.pop();
-    }
-    swap(){
-        this.arr = this.arr.reverse();
-    }
-    size(){
-        return this.arr.length
-    }
-    print(){
-        let str = "";
-        for(const item of this){
-            str += item.size + " ";
-        }
-        return str;
-    }
-    clear(){
-        this.arr.length = 0;
-    }
-    *[Symbol.iterator](){
-        yield * this.arr
-    }
-}
+const Peg = (() => {
+    const arr = Symbol('arr');
 
-let A = new Stack(),
-    B = new Stack(),
-    C = new Stack(),
-    moves = 0;
+    return class Stack {
+        constructor(){
+            this[arr] = []; 
+        }
+        push(val) {
+            this[arr].push(val);
+            return `Pushed ${val} on ${this}`;
+        }
+        pop() {
+            return this[arr].pop()
+        }
+        clear() {
+            this[arr].length = 0;
+            return `Just emptied the peg!`;
+        }
+        print() {
+            let str = "";
+            for (const item of this){
+                str += item.size + " ";
+            }
+            return str;
+        }
+        *[Symbol.iterator]() {
+            yield * this[arr];
+        }
+    }
+})();
+
+const A = new Peg(),
+      B = new Peg(),
+      C = new Peg();
+
+let movesCount = 0;
 
 const populate = (n, target) => 
     n == 0
     ? 'Peg has been populated!'
-    : (target.push(new Disc()), populate(n-1, target))
+    : (target.push(new Disc(n)), populate(n-1, target));
 
 const clearPegs = (...pegs) => {
     for(const peg of pegs){
         peg.clear();
     }
-    Disc.count = 0;
-    moves = 0;
-}
+};
+
+const resetMovesCount = () => {movesCount = 0};
 
 const move = (n, source, target, spare) => {
     if(n > 0){
-        moves += 1;
+        movesCount += 1;
         move(n-1, source, spare, target);
         target.push(source.pop());
         console.log(A.print());
@@ -66,13 +66,15 @@ const move = (n, source, target, spare) => {
         console.log('-----------------------------------------------');
         move(n-1, spare, target, source);
     }
-}
+};
 
 const play = (pegsNum) => {
 populate(pegsNum, A);
-A.swap();
 move(pegsNum, A, B, C);
-console.log(moves);
+console.log(movesCount);
 clearPegs(A,B,C);
-}
+resetMovesCount();
+};
+
+
 
