0e1638531755b50acb3c2ad741bcf956df1b00fe
Kamyl <kamillcybulski@gmail.com>
2017-05-02 00:33:45 +0200

    Improved timeouts management.

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 //some globals
-let A, B, C;
-   
+let A, B, C,
+    movesCounter = 0,
+    timeouts = [];
 
-//Initialization
-document.addEventListener("DOMContentLoaded", () => {
-    A = new Peg('peg1');
-    B = new Peg('peg2');
+
+
+//initialization
+document.addEventListener('DOMContentLoaded', () => {
+    A = new Peg('peg1'),
+    B = new Peg('peg2'),
     C = new Peg('peg3');
-    document.querySelector('.goButton')
-        .addEventListener("click", () => {
-            const num = document.getElementById('discNum').value;
-            playGame(num);
-        })
+
+    const goBtn = document.querySelector('.goButton'),
+          resetBtn = document.querySelector('.resetButton');
+
+    goBtn.addEventListener("click", () => {
+        const num = document.getElementById('discNum').value;
+        solve(num);
     });
+    resetBtn.addEventListener('click', () => {
+        reset();
+    });
+});
 
 //Data structures
 const Disc = (() => {
@@ -62,81 +71,68 @@ const Peg = (() => {
     }
 })();
 
-//play the game:
-const playGame = (num) => {
+//core functions
 
-    //state
-    let isRunning,
-        movesCounter = 0;
-
-    //core functions
-    const populate = (n, target) => 
+const populate = (n, target) => 
     n == 0
     ? 'Peg has been populated!'
     : (target.push(new Disc(n)), populate(n-1, target));
 
-    const visualizeDiscs = (stack) => {
-        for (let item of stack){
-            let disc = document.createElement('div');
-            disc.classList.add('disc');
-            disc.style.backgroundColor = item.color;
-            disc.style.width = `${item.size * 10}%`//item.size == 1 ? "20px" : `${item.size * 13}px`;
-            stack.peg.appendChild(disc);
-        }
-    };
+const visualizeDiscs = (stack) => {
+    for (let item of stack){
+        let disc = document.createElement('div');
+        disc.classList.add('disc');
+        disc.style.backgroundColor = item.color;
+        disc.style.width = `${item.size * 10}%`
+        stack.peg.appendChild(disc);
+    }
+};
 
-    const visualizeMove = (prevStack, newStack) => {
-        if(isRunning){
-            newStack.peg.appendChild(prevStack.peg.lastChild);
-        }
-    };
+const visualizeMove = (prevStack, newStack) => {
+      newStack.peg.appendChild(prevStack.peg.lastChild);  
+}
 
-    const move = (n, source, target, spare) => {
-        if (n > 0) {
-            move(n-1, source, spare, target);
-            setTimeout(() => {visualizeMove(source, target);},
+const move = (n, source, target, spare) => {
+    if (n > 0) {
+        move(n-1, source, spare, target);
+        timeouts.push(setTimeout(
+            () => {visualizeMove(source, target);},
             500*movesCounter++)
-            target.push(source.pop());
-            move(n-1, spare, target, source);
+        );
+        target.push(source.pop());
+        move(n-1, spare, target, source);
+    }
+};
+
+const clearPegs = (...stacks) => {
+    for (const stack of stacks) {
+        stack.clear();
+        while (stack.peg.childNodes.length > 0){
+            stack.peg.childNodes[0].remove();
         }
-    };
+    }
+};
 
-    const clearPegs = (...stacks) => {
-        for (const stack of stacks) {
-            stack.clear();
-            while (stack.peg.childNodes.length > 0){
-                stack.peg.childNodes[0].remove();
-            }
-        }
-    };
-
-    const resetMovesCounter = () => {movesCounter = 0;};
-
-    //Actually play the game:
-
-    //first, reset stuff:
-    clearPegs(A, B, C);
-    resetMovesCounter();
-
-    //Fix the buttons to stop previous game:
-    const goBtn = document.querySelector('.goButton'),
-          resetBtn = document.querySelector('.resetButton');
-    goBtn.addEventListener("click", () => {
-        isRunning = false;
+const resetVisualization = () => {
+    movesCounter = 0;
+    timeouts.map((timer)=> {
+        clearTimeout(timer);
     });
-    resetBtn.addEventListener("click", () => {
-        isRunning = false;
-        clearPegs(A,B,C);
-        resetMovesCounter();
-    })
+    timeouts = [];
+}
 
-    //Now, set up the scene and go:
-    populate(num, A);
+//button functionalities
+
+const reset = () => {
+    clearPegs(A, B, C);
+    resetVisualization();
+}
+
+const solve = (n) => {
+    reset();
+    populate(n, A);
     visualizeDiscs(A);
-    isRunning = true;
     setTimeout(() => {
-        move(num, A, B, C);
-    }, 2000)
-    
-
+        move(n, A, B, C);
+    }, 1000);
 }
